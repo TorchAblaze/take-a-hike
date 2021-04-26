6a750669075461be34739008609a66884464a639
Jesse White <jesse.white6@gmail.com>
2021-04-26 13:23:21 -0700

    add initial html markup and select state option.

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         new Dotenv(),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'template-repo',
+            title: 'Take A Hike',
             template: './src/index.html',
             inject: 'head'
         })
