6ef0fc0e4eb6edf0369dbfdefda83a19de91a9b1
Jesse White <jesse.white6@gmail.com>
2021-04-26 16:59:39 -0700

    add fucntionallity to park search based on state.

--- src/main.js
+++ src/main.js
@@ -4,9 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import NpsMainService from './js/nps-main-api.js';
 
-// function getElements(response) {
-//   $("#state-select").
-// }
+// const names = ["Jesse", "Marney", "Isaac", "Tiffany"]
+
+function getElements(response) {
+  let parkListHTML = ``;
+  if (response.data) {
+    response.data.forEach((element) => {
+      parkListHTML += (`<li>${element.fullName}</li>`);
+    });
+    $(".output").html(parkListHTML);
+  } else {
+    $(".output").text("that hit the else statement");
+    console.log(response);// we can see this! YAY! :D
+  }
+}
 
 
 $(document).ready(function () {
@@ -14,16 +25,16 @@ $(document).ready(function () {
     event.preventDefault();
 
     const selectedState = $("#state-select").val();
-    console.log(selectedState);
 
     NpsMainService.getPark(selectedState)
       .then(function (response) {
+        getElements(response);
         // getElements(response);
-        console.log("NpsMainService working");
-        console.log(response.limit);
-        response.data.forEach((element) => {
-          console.log(element.fullName);
-        });
+        let pleaseWork = response.data;
+        console.log(pleaseWork);
+        // pleaseWork.forEach((element) => {
+        //   console.log(element.fullName); 
       });
+
   });
 });
