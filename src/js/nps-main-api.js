4c803517366101504679e4bdf353fd68b09a50d5
Jesse White <jesse.white6@gmail.com>
2021-04-26 14:42:18 -0700

    add nps api callback.

---
+++ src/js/nps-main-api.js
@@ -0,0 +1,14 @@
+export default class NpsMainService {
+  static getPark(state) {
+    return fetch(`https://developer.nps.gov/api/v1/parks?q=${state}&api_key=${process.env.NPS_API_KEY}`)
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json;
+      })
+      .catch(function (error) {
+        return error.message;
+      })
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,28 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import NpsMainService from './js/nps-main-api.js';
+
+// function getElements(response) {
+//   $("#state-select").
+// }
+
 
 $(document).ready(function () {
+  $("#main-page").submit(function (event) {
+    event.preventDefault();
 
+    const selectedState = $("#state-select").val();
+    console.log(selectedState);
+
+    NpsMainService.getPark(selectedState)
+      .then(function (response) {
+        // getElements(response);
+        console.log("NpsMainService working");
+        console.log(response.limit);
+        response.data.forEach((element) => {
+          console.log(element.fullName);
+        });
+      });
+  });
 });
